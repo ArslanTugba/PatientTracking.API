2ca2cc67d9a18579a61fa3b086e225ddf29bc127
Tugba Arslan <arslantugba91@gmail.com>
2025-04-27 00:44:48 +0300

    fix: added important tracked files (Program.cs, Properties, .gitignore)

---
+++ Program.cs
@@ -0,0 +1,82 @@
+using Microsoft.EntityFrameworkCore;
+using PatientTracking.API.Data;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.OpenApi.Models;
+using PatientTracking.API.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Services
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "PatientTracking API", Version = "v1" });
+
+    // Swagger'da API için Authorization (Bearer Token) ekleyelim
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. Enter 'Bearer' [space] and your token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] { }
+        }
+    });
+});
+
+
+// Add DbContext for the application
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+builder.Services.AddScoped<PatientService>();
+
+
+// CORS Policy Configuration
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularClient", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200") // Your Angular frontend URL
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+// Apply the CORS policy
+app.UseCors("AllowAngularClient");
+
+// Middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "PatientTracking API v1");
+    });
+}
+
+
+// Default middlewares
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
