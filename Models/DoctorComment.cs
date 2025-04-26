7eec7c09054cd91c56dc933922a6f935993d01d9
Tugba Arslan <arslantugba91@gmail.com>
2025-04-27 00:41:58 +0300

    fix: reorganized and finalized project structure

---
+++ Models/DoctorComment.cs
@@ -0,0 +1,10 @@
+﻿namespace PatientTracking.API.Models
+{
+    public class DoctorComment
+    {
+        public int Id { get; set; } 
+        public int PatientId { get; set; } 
+        public string Comment { get; set; } 
+        public DateTime CreatedDate { get; set; } 
+    }
+}
